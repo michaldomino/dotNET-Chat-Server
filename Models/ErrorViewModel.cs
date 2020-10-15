3225d1f7ae5c092bfcbe29ddabde6f0d8193af6a
Michał Domino <michal.domino@yahoo.com>
2020-10-15 21:43:18 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotNET_Chat_Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
