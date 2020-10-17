d76fa301d8c560af9215bcffe554869d4775c6ff
Michał Domino <michal.domino@yahoo.com>
2020-10-17 15:23:16 +0200

    Renamed User to ApplicationUser, made ApplicationUser inherit from IdentityUser, add data sets for tables

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using dotNET_Chat_Server.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +14,8 @@ namespace dotNET_Chat_Server.Data
             : base(options)
         {
         }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Message> Messages { get; set; }
     }
 }
--- Models/Message.cs
+++ Models/Message.cs
@@ -10,12 +10,17 @@ namespace dotNET_Chat_Server.Models
     public class Message
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         public string Text { get; set; }
-        public DateTime Time { get; set; }
+        public DateTime CreationTime { get; set; }
 
-        [ForeignKey("UserId")]
-        public User User { get; set; }
-        public Guid UserId { get; set; }
+        [ForeignKey("AuthorId")]
+        public ApplicationUser Author { get; set; }
+        public Guid AuthorId { get; set; }
+
+        [ForeignKey("RecipientId")]
+        public ApplicationUser Recipient { get; set; }
+        public Guid RecipientId { get; set; }
     }
 }
