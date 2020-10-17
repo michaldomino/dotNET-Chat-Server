1acfcaba89872b518f63f1f7955481e127f4f195
Michał Domino <michal.domino@yahoo.com>
2020-10-17 16:23:45 +0200

    Add ApplicationRole, add building initial data

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace dotNET_Chat_Server.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -17,5 +17,25 @@ namespace dotNET_Chat_Server.Data
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, Guid>(this));
+            ApplicationUser user1 = new ApplicationUser();
+            ApplicationUser user2 = new ApplicationUser();
+            Message message = new Message()
+            {
+                Author = user1,
+                Recipient = user2,
+                Text = "abc",
+                CreationTime = DateTime.Now,
+            };
+
+            builder.Entity<ApplicationUser>().HasData(user1);
+            builder.Entity<ApplicationUser>().HasData(user2);
+            builder.Entity<Message>().HasData(message);
+
+            base.OnModelCreating(builder);
+        }
     }
 }
