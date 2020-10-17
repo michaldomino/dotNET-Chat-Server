54bd27f411a1c78089934c08e525d6f1bac91123
Michał Domino <michal.domino@yahoo.com>
2020-10-17 14:18:59 +0200

    Add User and Message models

---
+++ Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNET_Chat_Server.Models
+{
+    public class Message
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Time { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNET_Chat_Server.Models
+{
+    public class User
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
