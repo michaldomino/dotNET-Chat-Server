506e351c678f6bbb0c8a49e98b2744ebb7df1f4e
Michał Domino <michal.domino@yahoo.com>
2020-10-18 16:11:43 +0200

    Add ApplicationUserController

---
+++ Controllers/ApplicationUsersController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dotNET_Chat_Server.Data;
+using dotNET_Chat_Server.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace dotNET_Chat_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public ApplicationUsersController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            this.userManager = userManager;
+        }
+
+        // GET: api/ApplicationUsers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetApplicationUsers()
+        {
+            return await _context.ApplicationUsers.ToListAsync();
+        }
+
+        // GET: api/ApplicationUsers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApplicationUser>> GetApplicationUser(Guid id)
+        {
+            var applicationUser = await _context.ApplicationUsers.FindAsync(id);
+
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            return applicationUser;
+        }
+
+        // PUT: api/ApplicationUsers/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplicationUser(Guid id, ApplicationUser applicationUser)
+        {
+            if (id != applicationUser.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(applicationUser).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ApplicationUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ApplicationUsers
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<ApplicationUser>> PostApplicationUser(ApplicationUser applicationUser)
+        {
+            _context.ApplicationUsers.Add(applicationUser);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetApplicationUser", new { id = applicationUser.Id }, applicationUser);
+        }
+
+        // DELETE: api/ApplicationUsers/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ApplicationUser>> DeleteApplicationUser(Guid id)
+        {
+            var applicationUser = await _context.ApplicationUsers.FindAsync(id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            _context.ApplicationUsers.Remove(applicationUser);
+            await _context.SaveChangesAsync();
+
+            return applicationUser;
+        }
+
+        private bool ApplicationUserExists(Guid id)
+        {
+            return _context.ApplicationUsers.Any(e => e.Id == id);
+        }
+    }
+}
