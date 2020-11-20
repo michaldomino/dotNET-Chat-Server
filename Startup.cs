0b486537b6ec89b05e75dca541974f0e2176e881
Michał Domino <michal.domino@yahoo.com>
2020-11-20 20:20:08 +0100

    Removed redundant files

--- Startup.cs
+++ Startup.cs
@@ -26,9 +26,6 @@ namespace dotNET_Chat_Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddIdentity<ApplicationUser, ApplicationRole>()
-            //    .AddEntityFrameworkStores<ApplicationDbContext>()
-            //    .AddDefaultTokenProviders();
             JwtOptions jwtOptions = new JwtOptions();
             Configuration.Bind(nameof(JwtOptions), jwtOptions);
             services.AddSingleton(jwtOptions);
@@ -61,8 +58,6 @@ namespace dotNET_Chat_Server
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
-            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
